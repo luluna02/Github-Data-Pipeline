0f54736f4d0891e5b18719ce991c7eb037c82e1c
Lina Fadhli <133816965+luluna02@users.noreply.github.com>
2025-03-16 22:10:57 +0100

    updated docker-compose

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -55,7 +55,7 @@ def stream_github_data():
     """
     Stream GitHub repository data to Kafka.
     """
-    producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER], max_block_ms=5000)
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
     curr_time = time.time()
 
     while True:
@@ -65,19 +65,21 @@ def stream_github_data():
             repos = get_github_data()
             for repo in repos:
                 formatted_data = format_github_data(repo)
-                producer.send(KAFKA_TOPIC, json.dumps(formatted_data).encode('utf-8'))
+                producer.send('github_repos', json.dumps(formatted_data).encode('utf-8'))
                 logging.info(f"Published to Kafka: {formatted_data}")
         except Exception as e:
             logging.error(f'An error occurred: {e}')
             continue
 
 # Define the DAG
-with DAG('github_automation',
-         default_args=default_args,
-         schedule_interval='@daily',  # Run daily
-         catchup=False) as dag:
+#with DAG('github_automation',
+         #default_args=default_args,
+         #schedule_interval='@daily',  # Run daily
+         #catchup=False) as dag:
 
-    streaming_task = PythonOperator(
-        task_id='stream_github_data',
-        python_callable=stream_github_data
-    )
+    #streaming_task = PythonOperator(
+        #task_id='stream_github_data',
+       # python_callable=stream_github_data
+    #)
+
+stream_github_data()
